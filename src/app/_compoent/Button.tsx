ec83a821f5a11782871652428a76dbe2ab36ead9
jongbinchoi <jongbin985@naver.com>
2025-05-10 14:20:58 +0900

    test vercel2

--- src/app/_compoent/Button.tsx
+++ src/app/_compoent/Button.tsx
@@ -1,25 +1,18 @@
 "use client";
 
 import { useCallback, useState } from "react";
-import { createUser } from "../_action/user";
 
 export default function AddButton() {
 	const [name, setName] = useState("");
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
-	const handleSubmit = useCallback(async () => {
-		try {
-			const newUser = await createUser({
-				name,
-				email,
-				password,
-			});
-			console.log(newUser);
-		} catch (error) {
-			console.error(error);
-		}
-	}, [name, email, password]);
+	const handleSubmit = useCallback(
+		async function onClick() {
+			console.log(name, email, password);
+		},
+		[name, email, password]
+	);
 
 	return (
 		<>
@@ -28,27 +21,27 @@ export default function AddButton() {
 				name="name"
 				id="name"
 				value={name}
+				className="border border-amber-300"
 				onChange={(e) => setName(e.target.value)}
-				placeholder="Name"
 			/>
 			<input
 				type="email"
 				name="email"
 				id="email"
 				value={email}
+				className="border border-amber-300"
 				onChange={(e) => setEmail(e.target.value)}
-				placeholder="Email"
 			/>
 			<input
 				type="password"
 				name="password"
 				id="password"
 				value={password}
+				className="border border-amber-300"
 				onChange={(e) => setPassword(e.target.value)}
-				placeholder="Password"
 			/>
 			<button
-				onClick={handleSubmit}
+				onClick={() => handleSubmit()}
 				className="bg-blue-500 text-white p-2 rounded-md"
 			>
 				Click me
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import Image from "next/image";
 import AddButton from "./_compoent/Button";
-import { getUsers } from "./_action/user";
 
 export default async function Home() {
-	const users = await getUsers();
-	console.log(users);
 	return (
 		<div>
 			<h2 className="text-3xl font-bold underline">"hi"</h2>
